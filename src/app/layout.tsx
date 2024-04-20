af15a81ec2bd988f3aaa34b642255d4a7e2416f2
VGeorgiev <vgeorgiev@up2technology.com>
2024-04-20 23:05:46 +0300

    Update color variables in globals.css and change font in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
     <html lang='en'>
       <body
         className={cn(
-          'min-h-screen bg-background font-sans antialiased dark',
+          'min-h-screen bg-background font-poppins antialiased',
           domine.variable,
           poppins.variable
         )}
