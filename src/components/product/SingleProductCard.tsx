73d2b6b44c1c76dfa29a6f14670298271cacfa0a
VGeorgiev <vgeorgiev@up2technology.com>
2024-04-23 21:40:13 +0300

    Remove unused code

--- src/components/product/SingleProductCard.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-interface SingleProductCardProps {}
-
-const SingleProductCard: React.FC<SingleProductCardProps> = ({}) => {
-  return <div>SingleProductCard</div>;
-};
-
-export default SingleProductCard;
