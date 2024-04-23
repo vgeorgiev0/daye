93c8a4f0effb34c675fc84ddc848e0891a9469a9
VGeorgiev <vgeorgiev@up2technology.com>
2024-04-23 22:41:41 +0300

    Add product list to ProductsPage component

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -9,14 +9,38 @@ import {
 } from '@/components/ui/popover';
 import { ArrowDown } from 'lucide-react';
 
+const productList = [
+  'All Products',
+  'Period Care',
+  'CBD Tampons',
+  'Nude Tampons',
+  'Pads & Liners',
+  'Bundles',
+  'Vaginal Care',
+  'Balms',
+  'Accessories',
+];
+
 interface ProductsPageProps {}
 
 const ProductsPage: React.FC<ProductsPageProps> = async ({}) => {
   const products = await getDataFromApi();
 
   return (
-    <main className='flex flex-col items-center font-poppins justify-center w-full mt-32'>
-      <div className='flex w-full items-end justify-between bg-foreground p-3 text-typography mt-6'>
+    <main className='flex flex-col items-center justify-center text-typography font-poppins w-full mt-32'>
+      <div className='w-full lg:flex items-center justify-center bg-background h-10 hidden '>
+        <ul className='flex flex-row gap-6 text-typography font-poppins text-sm '>
+          {productList.map((product, index) => (
+            <li className='cursor-pointer relative' key={index}>
+              {product}
+              <span className='absolute -top-1 -right-3 text-xs text-slate-400'>
+                {Math.floor(Math.random() * (9 - 2 + 1)) + 2}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className='flex w-full items-end justify-between bg-background p-3 text-typography mt-6 lg:hidden border-b border-primary'>
         <Popover>
           <PopoverTrigger className='flex w-full items-center justify-between'>
             <p className='text-sm font-poppins '>All Products</p>
@@ -27,12 +51,13 @@ const ProductsPage: React.FC<ProductsPageProps> = async ({}) => {
               </span>
             </p>
           </PopoverTrigger>
-          <PopoverContent className='w-screen bg-foreground'>
+          <PopoverContent className='w-screen bg-background'>
             <ul className='flex flex-col w-full p-2 select-none'>
-              <li className='mb-3'>Tampons</li>
-              <li className='mb-3'>Pads</li>
-              <li className='mb-3'>Menstrual Cups</li>
-              <li className=''>Period Underwear</li>
+              {productList.map((product, index) => (
+                <li key={index} className='mb-3'>
+                  {product}
+                </li>
+              ))}
             </ul>
           </PopoverContent>
         </Popover>
--- src/components/auth/AuthenticationForm.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-interface AuthenticationFormProps {}
-
-const AuthenticationForm: React.FC<AuthenticationFormProps> = ({}) => {
-  return <div>AuthenticationForm</div>;
-};
-
-export default AuthenticationForm;
